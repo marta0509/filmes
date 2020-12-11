eaca21461c5e10f851044e8b05b122bfd0095174
marta <marta@LAPTOP-0MP64H3N.local>
2020-12-11 20:17:30 +0000

    sistema CRUD - read- details

--- filmes_show.php
+++ filmes_show.php
@@ -4,11 +4,11 @@
 		if (!isset($_GET['filme']) || !is_numeric($_GET['filme'])) {
 			echo '<script>alert("Erro ao abrir Filme");</script>';
 			echo "Aguarde um momento. A reecaminhar página";
-			header("refresh:5;url=read/index.php");
+			header("refresh:5;url=index.php");
 			exit();		
 		}	
 		$idFilme=$_GET['filme'];
-		$con=new mysli("localhost","root","","filmes");
+		$con=new mysqli("localhost","root","","filmes");
 
 		if ($con->connect_errno!=0)
 		{
@@ -17,13 +17,14 @@
 		}
 		else
 		{
-			$sql='select * from filmes where id_fime=?';
+			$sql='select * from filmes where id_filme=?';
 			$stm =$con->prepare($sql);
 			if ($stm!=false)
 			{
 				$stm->bind_param('i',$idFilme);
 				$stm->execute();
-				$res=$res->fetchassoc();
+				$res=$stm->get_result();
+				$filme=$res->fetch_assoc();
 				$stm->close();
 			}
 		else
@@ -37,3 +38,33 @@
 		}
 	}
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Detalhes</title>
+</head>
+<body>
+	<H1>Detalhes do filme</H1>
+	<?php
+		if (isset($filme)) {
+			echo "<br>";
+			echo $filme['titulo'];
+			echo "<br>";
+			echo utf8_encode($filme['sinopse']);
+			echo "<br>";
+			echo $filme['data_lancamento'];
+			echo "<br>";
+			echo $filme['idioma'];
+			echo "<br>";
+			echo $filme['quantidade'];
+			echo "<br>";
+		}
+		else
+		{
+			echo "<h2>Parece que o filme selecionado não existe.<br>Continue a sua seleção.</h2>";
+		}
+	?>
+</body>
+</html>
