55fc7b14c66863858228e40ecd81663c2a65daf2
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2021-01-07 12:19:56 +0000

    index realizadores

---
+++ index_realizadores.php
@@ -0,0 +1,51 @@
+<?php
+	$con=new mysqli("localhost","root","","filmes");
+	if($con->connect_errno!=0)
+	{
+		echo "Ocorreu um erro no acesso à base de dados".$con->connect_error;
+		exit;
+	}
+	else
+	{
+?>
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<meta charset="ISO-8859-1">
+			<title>Realizadores</title>
+			<link href="CSS/bootstrap.min.css" rel="stylesheet" >
+			<link rel="stylesheet" href="CSS/jumbotrom.css">
+			<link rel="stylesheet" href="CSS/all.min.css">
+		</head>
+		<body style="background: #BFFAF7">
+			<h1 style="color: darkblue">Lista de Realizadores</h1>
+			<br>
+			<?php
+				$stm=$con->prepare('select * from realizadores');
+				$stm->execute();
+				$res=$stm->get_result();
+				while ($resultado=$res->fetch_assoc())
+				{
+					echo '<a href="edit_realizadores.php?realizador='.$resultado['id_realizador'].'"><i style="color:blue" class="fas fa-pen"></i>';
+					echo '</a>'.' ';
+					echo '<a style="color:black" href="realizadores_show.php?realizador='.$resultado['id_realizador'].'"><i style="color:blue" class="far fa-eye"></i>';
+					echo '</a>'.' ';
+					echo '<a href="delete_realizadores.php?realizador='.$resultado['id_realizador'].'"><i style="color:blue" class="fas fa-eraser"></i>';
+					echo '</a>'.' ';
+					echo '<b>'.$resultado['nome'].'</b>';
+					echo "<br>";
+				}
+				$stm->close();
+			?>
+			<br>
+			<a href="create_realizadores.php">Criar um novo registo</a>
+		<br>
+		
+		<script src="JS/jquery-3.5.1.min.js"></script>
+		<script src="JS/bootstrap.min.js"></script>
+		<script src="JS/all.min.js"></script>
+		</body>
+		</html>
+	<?php
+		}//end if -if($con->connect_errno!=0)
+	?>
